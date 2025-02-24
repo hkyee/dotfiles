870e3072113df32da9e5aa8b401dee6350e08396
Herr Ken Yee <herrkenyee@gmail.com>
2025-02-24 22:30:07 +0800

    24 Feb 2025

--- nvim/lua/j/plugins/nvim-treesitter.lua
+++ nvim/lua/j/plugins/nvim-treesitter.lua
@@ -1,14 +1,22 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function () 
+	config = function()
 		local configs = require("nvim-treesitter.configs")
 
-	      	configs.setup({
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "rust", "go", "cpp", "javascript", "html" },
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },  
-	})
-	end
+		configs.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "rust", "go", "cpp", "javascript", "html" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+
+			-- vim matchup integration
+			matchup = {
+				enable = true, -- mandatory, false will disable the whole extension
+				disable = { "c", "ruby" }, -- optional, list of language that will be disabled
+				-- [options]
+				-- vim matchup integration End
+			},
+		})
+	end,
 }
